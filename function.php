6ed33eca36ee9c15a944d4743cc92b66e37ba0b1
kevinct9 <64075226+kevinct9@users.noreply.github.com>
2020-06-29 02:20:19 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,4 +1,4 @@
--6.923201,107.618538<?php
+-6.886439,107.580725<?php
 
 function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
 
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location:-6.923201,107.618538";
+$header[] = "X-Location:-6.886439,107.580725";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
